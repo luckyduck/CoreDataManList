11ab23dd770bfac352e8447dbd0b2f5451d5a9be
Stefan Jölly <stefan@joelly.de>
2016-09-04 14:00:44 +0200

    Added everything in Folder

---
+++ git_Manuelle_Liste_01/git_Manuelle_Liste_01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git_Manuelle_Liste_01
+//
+//  Created by Stefan Jölly on 04.09.16.
+//  Copyright © 2016 Stefan Jölly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
